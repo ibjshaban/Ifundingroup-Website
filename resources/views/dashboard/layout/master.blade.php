cd1288d1a343fd9d2389feccc8f5441cbf76e9b0
ibrahim <ibrahim_shaban_1996@hotmail.com>
2019-12-17 01:10:15 +0200

    Small tasks in front-end

--- resources/views/dashboard/layout/master.blade.php
+++ resources/views/dashboard/layout/master.blade.php
@@ -28,11 +28,15 @@
     <link rel="stylesheet" href="{{ asset('dashboard-assets/modules/bootstrap-social/bootstrap-social.css') }}">
 
     <!-- Template CSS -->
-    <link rel="stylesheet" href="{{ asset('dashboard-assets/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('dashboard-assets/css/components.css') }}">
+    <link rel="stylesheet" href="{{ asset('dashboard-assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('dashboard-assets/css/custom.css') }}">
     {{--
       <link rel="stylesheet" href="{{ mix('css/style.css') }}">
     --}}
+    <style>
+
+    </style>
     @yield('headerCSS')
 </head>
 
@@ -45,73 +49,158 @@
                 <ul class="navbar-nav mr-3">
                     <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a>
                     </li>
-                    <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i
-                                class="fas fa-search"></i></a></li>
+
+                        <div class="dropdown d-inline mr-2">
+                            <button class="btn btn-success dropdown-toggle" style="background: #73eb29" type="button"
+                                    id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false">
+                                Personal activity
+                            </button>
+                            <div class="dropdown-menu mt-3 personal" x-placement="bottom-start" id="personal"
+                                 style="position: absolute; transform: translate3d(0px, 28px, 0px); top: 0px; left: 0px; will-change: transform; width: 450px;" >
+                                    <div class="list-group">
+                                        <a href="#"
+                                           class="list-group-item list-group-item-action flex-column align-items-start">
+                                            <div class="d-flex w-100 justify-content-between">
+
+                                                <h5 class="mb-1">
+                                                    <figure class="avatar mr-1 avatar-sm">
+                                                        <img src="{{ asset('dashboard-assets/img/avatar/avatar-1.png') }}" alt="...">
+                                                    </figure>
+                                                    John</h5>
+                                                <small>3 days ago</small>
+                                            </div>
+                                            <p class="mb-1">Reached level: 18</p>
+                                            <small>20/11/2019 - 9:45 am</small>
+                                        </a>
+                                        <a href="#"
+                                           class="list-group-item list-group-item-action flex-column align-items-start">
+                                            <div class="d-flex w-100 justify-content-between">
+
+                                                <h5 class="mb-1">
+                                                    <figure class="avatar mr-1 avatar-sm">
+                                                        <img src="{{ asset('dashboard-assets/img/avatar/avatar-1.png') }}" alt="...">
+                                                    </figure>
+                                                    John</h5>
+                                                <small>3 days ago</small>
+                                            </div>
+                                            <p class="mb-1">Reached level: 18</p>
+                                            <small>20/11/2019 - 9:45 am</small>
+                                        </a>
+                                        <a href="#"
+                                           class="list-group-item list-group-item-action flex-column align-items-start">
+                                            <div class="d-flex w-100 justify-content-between">
+
+                                                <h5 class="mb-1">
+                                                    <figure class="avatar mr-1 avatar-sm">
+                                                        <img src="{{ asset('dashboard-assets/img/avatar/avatar-1.png') }}" alt="...">
+                                                    </figure>
+                                                    John</h5>
+                                                <small>3 days ago</small>
+                                            </div>
+                                            <p class="mb-1">Reached level: 18</p>
+                                            <small>20/11/2019 - 9:45 am</small>
+                                        </a>
+                                        <a href="#"
+                                           class="list-group-item list-group-item-action flex-column align-items-start">
+                                            <div class="d-flex w-100 justify-content-between">
+
+                                                <h5 class="mb-1">
+                                                    <figure class="avatar mr-1 avatar-sm">
+                                                        <img src="{{ asset('dashboard-assets/img/avatar/avatar-1.png') }}" alt="...">
+                                                    </figure>
+                                                    John</h5>
+                                                <small>3 days ago</small>
+                                            </div>
+                                            <p class="mb-1">Reached level: 18</p>
+                                            <small>20/11/2019 - 9:45 am</small>
+                                        </a>
+                                        <a href="#"
+                                           class="list-group-item list-group-item-action flex-column align-items-start">
+                                            <div class="d-flex w-100 justify-content-between">
+
+                                                <h5 class="mb-1">
+                                                    <figure class="avatar mr-1 avatar-sm">
+                                                        <img src="{{ asset('dashboard-assets/img/avatar/avatar-1.png') }}" alt="...">
+                                                    </figure>
+                                                    John</h5>
+                                                <small>3 days ago</small>
+                                            </div>
+                                            <p class="mb-1">Reached level: 18</p>
+                                            <small>20/11/2019 - 9:45 am</small>
+                                        </a>
+
+                                    </div>
+                            </div>
+                        </div>
+                    </li>
                 </ul>
+
             </form>
             <ul class="navbar-nav navbar-right">
 
-                    <div class="dropdown-menu dropdown-list dropdown-menu-right">
-                        <div class="dropdown-header">Notifications
-                            <div class="float-right">
-                                <a href="#">Mark All As Read</a>
-                            </div>
-                        </div>
-                        <div class="dropdown-list-content dropdown-list-icons">
-                            <a href="#" class="dropdown-item dropdown-item-unread">
-                                <div class="dropdown-item-icon bg-primary text-white">
-                                    <i class="fas fa-code"></i>
-                                </div>
-                                <div class="dropdown-item-desc">
-                                    Template update is available now!
-                                    <div class="time text-primary">2 Min Ago</div>
-                                </div>
-                            </a>
-                            <a href="#" class="dropdown-item">
-                                <div class="dropdown-item-icon bg-info text-white">
-                                    <i class="far fa-user"></i>
-                                </div>
-                                <div class="dropdown-item-desc">
-                                    <b>You</b> and <b>Dedik Sugiharto</b> are now friends
-                                    <div class="time">10 Hours Ago</div>
-                                </div>
-                            </a>
-                            <a href="#" class="dropdown-item">
-                                <div class="dropdown-item-icon bg-success text-white">
-                                    <i class="fas fa-check"></i>
-                                </div>
-                                <div class="dropdown-item-desc">
-                                    <b>Kusnaedi</b> has moved task <b>Fix bug header</b> to <b>Done</b>
-                                    <div class="time">12 Hours Ago</div>
-                                </div>
-                            </a>
-                            <a href="#" class="dropdown-item">
-                                <div class="dropdown-item-icon bg-danger text-white">
-                                    <i class="fas fa-exclamation-triangle"></i>
-                                </div>
-                                <div class="dropdown-item-desc">
-                                    Low disk space. Let's clean it!
-                                    <div class="time">17 Hours Ago</div>
-                                </div>
-                            </a>
-                            <a href="#" class="dropdown-item">
-                                <div class="dropdown-item-icon bg-info text-white">
-                                    <i class="fas fa-bell"></i>
-                                </div>
-                                <div class="dropdown-item-desc">
-                                    Welcome to Stisla template!
-                                    <div class="time">Yesterday</div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="dropdown-footer text-center">
-                            <a href="#">View All <i class="fas fa-chevron-right"></i></a>
+                <div class="dropdown-menu dropdown-list dropdown-menu-right">
+                    <div class="dropdown-header">Notifications
+                        <div class="float-right">
+                            <a href="#">Mark All As Read</a>
                         </div>
                     </div>
+                    <div class="dropdown-list-content dropdown-list-icons">
+                        <a href="#" class="dropdown-item dropdown-item-unread">
+                            <div class="dropdown-item-icon bg-primary text-white">
+                                <i class="fas fa-code"></i>
+                            </div>
+                            <div class="dropdown-item-desc">
+                                Template update is available now!
+                                <div class="time text-primary">2 Min Ago</div>
+                            </div>
+                        </a>
+                        <a href="#" class="dropdown-item">
+                            <div class="dropdown-item-icon bg-info text-white">
+                                <i class="far fa-user"></i>
+                            </div>
+                            <div class="dropdown-item-desc">
+                                <b>You</b> and <b>Dedik Sugiharto</b> are now friends
+                                <div class="time">10 Hours Ago</div>
+                            </div>
+                        </a>
+                        <a href="#" class="dropdown-item">
+                            <div class="dropdown-item-icon bg-success text-white">
+                                <i class="fas fa-check"></i>
+                            </div>
+                            <div class="dropdown-item-desc">
+                                <b>Kusnaedi</b> has moved task <b>Fix bug header</b> to <b>Done</b>
+                                <div class="time">12 Hours Ago</div>
+                            </div>
+                        </a>
+                        <a href="#" class="dropdown-item">
+                            <div class="dropdown-item-icon bg-danger text-white">
+                                <i class="fas fa-exclamation-triangle"></i>
+                            </div>
+                            <div class="dropdown-item-desc">
+                                Low disk space. Let's clean it!
+                                <div class="time">17 Hours Ago</div>
+                            </div>
+                        </a>
+                        <a href="#" class="dropdown-item">
+                            <div class="dropdown-item-icon bg-info text-white">
+                                <i class="fas fa-bell"></i>
+                            </div>
+                            <div class="dropdown-item-desc">
+                                Welcome to Stisla template!
+                                <div class="time">Yesterday</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="dropdown-footer text-center">
+                        <a href="#">View All <i class="fas fa-chevron-right"></i></a>
+                    </div>
+                </div>
                 </li>
                 <li class="dropdown"><a href="#" data-toggle="dropdown"
                                         class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                        <img alt="image" src="{{ is_null($user->avatar)?url('dashboard-assets/img/avatar/avatar-1.png'):url('storage/'.$user->avatar) }}"
+                        <img alt="image"
+                             src="{{ is_null($user->avatar)?url('dashboard-assets/img/avatar/avatar-1.png'):url('storage/'.$user->avatar) }}"
                              class="rounded-circle mr-1">
                         <div class="d-sm-none d-lg-inline-block">Hi, @yield('username')</div>
                     </a>
